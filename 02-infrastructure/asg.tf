fe810deff08fc5ca6e2276e1f2b40ae125449079
Mike Monaco <mamonaco1973@gmail.com>
2024-11-26 15:46:19 -0500

    add scale up/down policies

--- 02-infrastructure/asg.tf
+++ 02-infrastructure/asg.tf
@@ -1,6 +1,64 @@
 
 # Create Auto Scaling Group
 
+# Create CloudWatch Alarm for scaling up when CPUUtilization > 80%
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "HighCPUUtilization"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "Scale up if CPUUtilization > 80% for 2 minutes"
+  actions_enabled     = true
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.challenge_flask_asg.name
+  }
+
+  alarm_actions = [aws_autoscaling_policy.scale_up_policy.arn]
+}
+
+# Scaling policy for scaling up
+resource "aws_autoscaling_policy" "scale_up_policy" {
+  name                   = "scale-up-policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 120
+  autoscaling_group_name = aws_autoscaling_group.challenge_flask_asg.name
+}
+
+# Create CloudWatch Alarm for scaling down when CPUUtilization < 5%
+resource "aws_cloudwatch_metric_alarm" "cpu_low" {
+  alarm_name          = "LowCPUUtilization"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 5
+  alarm_description   = "Scale down if CPUUtilization < 5% for 2 minutes"
+  actions_enabled     = true
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.challenge_flask_asg.name
+  }
+
+  alarm_actions = [aws_autoscaling_policy.scale_down_policy.arn]
+}
+
+# Scaling policy for scaling down
+resource "aws_autoscaling_policy" "scale_down_policy" {
+  name                   = "scale-down-policy"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 120
+  autoscaling_group_name = aws_autoscaling_group.challenge_flask_asg.name
+}
+
 resource "aws_autoscaling_group" "challenge_flask_asg" {
   launch_template {
     id      = aws_launch_template.challenge_launch_template.id
