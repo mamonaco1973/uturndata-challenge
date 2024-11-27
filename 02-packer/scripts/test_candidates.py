b5bff4f2d52e96c0680e3f3d0c0fad9fa3228d34
Ubuntu <ubuntu@ip-10-1-0-11.us-east-2.compute.internal>
2024-11-27 18:25:27 +0000

    parameterize test python program

--- 02-packer/scripts/test_candidates.py
+++ 02-packer/scripts/test_candidates.py
@@ -1,40 +1,48 @@
+#!/usr/bin/python3
+
 import urllib.request
+import sys
+
+# Check if the command line argument is provided
+if len(sys.argv) != 2:
+    print("Usage: python test_candidates.py <base_url>")
+    sys.exit(1)
+
+# Get the base URL from the command line argument
+base_url = sys.argv[1]
 
 try:
-    with urllib.request.urlopen('http://localhost:8000/gtg') as gtg:
+    with urllib.request.urlopen(f'http://{base_url}/gtg') as gtg:
         if gtg.code == 200:
             print(u'\033[90m\N{check mark}\033[0m good to go passed')
         else:
             raise Exception
 except Exception as ex:
-    print(u'\033[91m\N{ballot x}\033[0m good to go failed:', ex.reason)
-
+    print(u'\033[91m\N{ballot x}\033[0m good to go failed:', getattr(ex, 'reason', str(ex)))
 
 try:
-    with urllib.request.urlopen('http://localhost:8000/candidate/John%20Smith', data=b'test') as insert:
+    with urllib.request.urlopen(f'http://{base_url}/candidate/John%20Smith', data=b'test') as insert:
         if insert.code == 200:
             print(u'\033[90m\N{check mark}\033[0m insert passed')
         else:
             raise Exception
 except Exception as ex:
-    print(u'\033[91m\N{ballot x}\033[0m insert failed:', ex.reason)
-
+    print(u'\033[91m\N{ballot x}\033[0m insert failed:', getattr(ex, 'reason', str(ex)))
 
 try:
-    with urllib.request.urlopen('http://localhost:8000/candidate/John%20Smith') as verify:
+    with urllib.request.urlopen(f'http://{base_url}/candidate/John%20Smith') as verify:
         if verify.code == 200:
             print(u'\033[90m\N{check mark}\033[0m verification passed')
         else:
             raise Exception(code=verify.code)
 except Exception as ex:
-    print(u'\033[91m\N{ballot x}\033[0m verification failed:', ex.reason)
+    print(u'\033[91m\N{ballot x}\033[0m verification failed:', getattr(ex, 'reason', str(ex)))
 
 try:
-    with urllib.request.urlopen('http://localhost:8000/candidates') as list:
+    with urllib.request.urlopen(f'http://{base_url}/candidates') as list:
         if list.code == 200:
             print(u'\033[90m\N{check mark}\033[0m candidate list passed')
         else:
             raise Exception
 except Exception as ex:
-    print(u'\033[91m\N{ballot x}\033[0m candidate list failed:', ex.reason)
-
+    print(u'\033[91m\N{ballot x}\033[0m candidate list failed:', getattr(ex, 'reason', str(ex)))
